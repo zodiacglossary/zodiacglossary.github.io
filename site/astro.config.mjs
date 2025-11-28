55a6285dbc131c38d7959488a22c944a55185620
Kierán Meinhardt <kmein@posteo.de>
2025-11-28 12:39:29 +0100

    add search component to index

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zodiacglossary.github.io',
-  integrations: [react()],
+  integrations: [react(), mdx()],
 
   vite: {
     plugins: []
