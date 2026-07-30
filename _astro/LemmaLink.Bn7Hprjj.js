7e045272417eca14fd342373cecc40d103ee70c7
ruturhan <ruturhan@users.noreply.github.com>
2026-07-30 09:52:32 +0000

    deploy: 9d9d3e2824a7a7be97c519ee25f0a59e8e351c6f

---
+++ _astro/LemmaLink.Bn7Hprjj.js
@@ -0,0 +1,9 @@
+import"./index.Cd_vQiNd.js";var d={exports:{}},s={};/**
+ * @license React
+ * react-jsx-runtime.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var x;function h(){if(x)return s;x=1;var r=Symbol.for("react.transitional.element"),a=Symbol.for("react.fragment");function e(o,t,i){var u=null;if(i!==void 0&&(u=""+i),t.key!==void 0&&(u=""+t.key),"key"in t){i={};for(var l in t)l!=="key"&&(i[l]=t[l])}else i=t;return t=i.ref,{$$typeof:r,type:o,key:u,ref:t!==void 0?t:null,props:i}}return s.Fragment=a,s.jsx=e,s.jsxs=e,s}var c;function p(){return c||(c=1,d.exports=h()),d.exports}var n=p();const g=r=>/\p{Lu}(?!\p{Ll})/u.test(r??""),j=r=>/[0-9?]/.test(r??"");function R({lemma:r}){if(!r||!r.lemma_id||!(r.original||r.transliteration)||!r.primary_meaning)return n.jsx(n.Fragment,{children:`(unknown lemma ${r?.lemma_id})`});if(r.language_id===1){const o=g(r.original)||j(r.original);return n.jsxs(n.Fragment,{children:[n.jsxs("a",{href:`/lemma/${r.lemma_id}`,children:["〈",r.transliteration?n.jsx("span",{className:"translit-headword",children:r.transliteration}):r.original,"〉"]})," ",o&&r.transliteration&&n.jsxs(n.Fragment,{children:[n.jsx("span",{className:"logogram",children:r.original})," "]}),"‘",r.primary_meaning,"’"]})}const a=r.original||r.transliteration,e=r.original&&r.transliteration;return n.jsxs(n.Fragment,{children:[n.jsxs("a",{href:`/lemma/${r.lemma_id}`,children:["〈",r.original?a:n.jsx("span",{className:"transliteration",children:a}),"〉"]})," ",e&&n.jsxs(n.Fragment,{children:[n.jsx("span",{className:"transliteration",children:r.transliteration})," "]}),"‘",r.primary_meaning,"’"]})}export{R as L,n as j};
